8cdd66b7cbb6257e11e92d945531849c36f239f6
natty-7 <nateshrajofficial@gmail.com>
2025-01-05 11:48:10 +0530

    adding azurerm in provider.tf

--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -14,16 +14,16 @@ provider "azurerm" {
   features {}
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
+# # Create a resource group
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources"
+#   location = "West Europe"
+# }
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
-}
+# # Create a virtual network within the resource group
+# resource "azurerm_virtual_network" "example" {
+#   name                = "example-network"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   address_space       = ["10.0.0.0/16"]
+# }
