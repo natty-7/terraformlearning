c72aaa4008fbd34bbecea7c081ee0976013aba5b
natty-7 <nateshrajofficial@gmail.com>
2025-01-07 23:48:36 +0530

    adding tags variable in vnet module

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -4,5 +4,5 @@ resource "azurerm_virtual_network" "vnet" {
  address_space = [var.vnet_address_space]
  location = var.location
  resource_group_name = var.resource_group_name
-
+ tags = var.tags
 }
