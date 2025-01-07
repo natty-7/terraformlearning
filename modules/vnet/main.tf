9a6df2927966d12f02838c98fa102309b2d5543f
natty-7 <nateshrajofficial@gmail.com>
2025-01-07 23:46:50 +0530

    adding code on vnet module

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,4 +1,7 @@
 #vnet module requires vnet, subnet, resource group, cidr range and location
 resource "azurerm_virtual_network" "vnet" {
-  
+ name = var.vnet_name
+ address_space = [var.vnet_address_space]
+ location = var.location
+
 }
