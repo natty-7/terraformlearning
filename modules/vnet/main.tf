58f563daa4495758312f1d6788ced33b4b2ed1a4
natty-7 <nateshrajofficial@gmail.com>
2025-01-08 17:30:58 +0530

    adding subnet code under vnet module

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -9,6 +9,11 @@ resource "azurerm_virtual_network" "vnet" {
 
 #create subnets within the virutal network
 resource "azurerm_subnet" "subnet" {
-  
-  
+  #create a subnet for each item in the subnets list
+  count = length(var.subnets)
+  name = var.subnets[count.index].name
+  resource_group_name = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes = [var.subnets[count.index].address_prefix]
+
 }
